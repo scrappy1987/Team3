8832fdc05abc2cd2f2c48e9a69ce544da5ad5b5b
Kimberley McGill <k_mcgill@hotmail.co.uk>
2017-01-30 15:03:12 +0000

    QC-72-create-certification-rating-entity

---
+++ src/main/java/com/qa/cinema/CertRating.java
@@ -0,0 +1,49 @@
+/** 
+ * Package: com.qa.cinema
+ * File: CertRating
+ * @Author: Kimberley McGill
+ * 
+ * Description: The CertRating class to explain the ratings
+ * for the films shown in our cinema
+ * Each rating will have a ratingID, a ratingTitle and a 
+ * ratingDescription.
+ */
+
+package com.qa.cinema;
+
+public class CertRating {
+
+	// variables
+	private int ratingID;
+	private String ratingTitle;
+	private String ratingDescription;
+	
+	
+	// getters and setters
+	public int getRatingID() {
+		return ratingID;
+	}
+	
+	public void setRatingID(int ratingID) {
+		this.ratingID = ratingID;
+	}
+	
+	public String getRatingTitle() {
+		return ratingTitle;
+	}
+	
+	public void setRatingTitle(String ratingTitle) {
+		this.ratingTitle = ratingTitle;
+	}
+	
+	public String getRatingDescription() {
+		return ratingDescription;
+	}
+	 
+	public void setRatingDescription(String ratingDescription) {
+		this.ratingDescription = ratingDescription;
+	}
+	
+	
+	
+}
