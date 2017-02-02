4d3096a3134945c3100268ac6237756e7f964185
PiratedSoftwareCo <jackjack.iot@gmail.com>
2017-02-02 15:12:47 +0000

    QC-98 : Deleted whitespace

--- src/main/java/com/qa/cinema/AddressService.java
+++ src/main/java/com/qa/cinema/AddressService.java
@@ -28,20 +28,20 @@ public class AddressService {
 	public void updateAddress(Address newAddress, Address oldAddress){
 		Address tmp = (Address)em.createQuery("SELECT * FROM Address WHERE address_line1 = '" + oldAddress.getLine1() + "' AND address_postcode ='" + oldAddress.getPostCode() + "';").getSingleResult();
 		em.createQuery("UPDATE Address SET address_line1 ='" +
-						newAddress.getLine1() +
-						"', address_line2 = '" +
-						newAddress.getLine2() +
-						"', address_county = '" + 
-						newAddress.getCounty() +
-						"', address_country ='" +
-						newAddress.getCountry() +
-						"', address_postCode = '"+
-						newAddress.getPostCode() +
-						"' WHERE address_line1 ='" + 
-						tmp.getLine1() +
-						"' AND address_postcode ='" +
-						tmp.getPostCode() +"';"
-					);
+			newAddress.getLine1() +
+			"', address_line2 = '" +
+			newAddress.getLine2() +
+			"', address_county = '" + 
+			newAddress.getCounty() +
+			"', address_country ='" +
+			newAddress.getCountry() +
+			"', address_postCode = '"+
+			newAddress.getPostCode() +
+			"' WHERE address_line1 ='" + 
+			tmp.getLine1() +
+			"' AND address_postcode ='" +
+			tmp.getPostCode() +"';"
+		);
 	}
 	
 	@POST
