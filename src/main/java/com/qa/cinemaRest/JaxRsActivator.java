163f7a4a2581c97ce1ab5967f73665466c08398c
PiratedSoftwareCo <jackjack762@gmail.com>
2017-02-03 12:21:00 +0000

    Update and rename src/main/java/com/qa/cinema/rest/JaxRsActivator.java to src/main/java/com/qa/cinemaRest/JaxRsActivator.java

--- src/main/java/com/qa/cinemaRest/JaxRsActivator.java
+++ src/main/java/com/qa/cinemaRest/JaxRsActivator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.qa.cinema.rest;
+package com.qa.cinemaRest;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
