32b7ea0810cfd22d2e4caf6da7ef25b5b9663dc7
PiratedSoftwareCo <jackjack762@gmail.com>
2017-02-03 12:22:38 +0000

    Update and rename src/main/java/com/qa/cinema/util/Resources.java to src/main/java/com/qa/cinemaUtil/Resources.java

--- src/main/java/com/qa/cinemaUtil/Resources.java
+++ src/main/java/com/qa/cinemaUtil/Resources.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.qa.cinema.util;
+package com.qa.cinemaUtil;
 
 import java.util.logging.Logger;
 
