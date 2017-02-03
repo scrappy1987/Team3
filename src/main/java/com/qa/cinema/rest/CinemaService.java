186c18fb4789acb1afdc903eea0e28dcc1b10953
Duncan McRobert <dmcrobert@live.co.uk>
2017-02-03 09:39:21 +0000

    QC-79-Creat-CinemaEntity
    Commiting priot to branch switch, minor changes

--- src/main/java/com/qa/cinema/rest/CinemaService.java
+++ src/main/java/com/qa/cinema/rest/CinemaService.java
@@ -1,10 +1,9 @@
 package com.qa.cinema.rest;
 
 import java.util.List;
-
+import javax.persistence.EntityManager;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
